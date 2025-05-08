5af20f7a4de347f5f101aa4b811e0e1bceae8451
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-05-08 16:37:16 +0500

    region done

--- src/region/dto/create-region.dto.ts
+++ src/region/dto/create-region.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateRegionDto {
+    @IsString()
     @ApiProperty({example: "Yer"})
     name: string
 }
