91d81400630f5f4dbe4ab33423d69f071ca61666
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-05-08 16:31:46 +0500

    changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-<<<<<<< HEAD
+import { RegionModule } from './region/region.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [PrismaModule, RestaurantModule],
-=======
-import { RegionModule } from './region/region.module';
-
-@Module({
-  imports: [PrismaModule, RegionModule],
->>>>>>> a40aee940557558693fb67046dce0aa3c16f418c
+  imports: [PrismaModule, RegionModule, RestaurantModule],
   controllers: [AppController],
   providers: [AppService],
 })
