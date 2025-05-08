6f69b98a602904ab65f13c1c25417ff5c33d5cbf
Abror-150 <urazalievv.abror@gmail.com>
2025-05-08 16:38:46 +0500

    conflikt

--- src/region/region.service.ts
+++ src/region/region.service.ts
@@ -5,61 +5,60 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RegionService {
+  constructor(private readonly prisma: PrismaService) {}
 
-  constructor(private readonly prisma: PrismaService){}
-
-  async regionFinder(name: string){
-    const one = await this.prisma.region.findFirst({where: {name}})
-    return one
+  async regionFinder(name: string) {
+    const one = await this.prisma.region.findFirst({ where: { name } });
+    return one;
   }
 
   async create(data: CreateRegionDto) {
     try {
-      const existRegion = await this.regionFinder(data.name)
-      if(existRegion){
-        throw new BadRequestException("This Region exists")
+      const existRegion = await this.regionFinder(data.name);
+      if (existRegion) {
+        throw new BadRequestException('This Region exists');
       }
 
-      const one = await this.prisma.region.create({data})
-      return one
+      const one = await this.prisma.region.create({ data });
+      return one;
     } catch (error) {
-      throw new BadRequestException("Region create error")
+      throw new BadRequestException('Region create error');
     }
   }
 
   async findAll() {
     try {
-      const one = await this.prisma.region.findMany()
+      const one = await this.prisma.region.findMany();
       return one;
     } catch (error) {
-      throw new BadRequestException("Region findall error")
+      throw new BadRequestException('Region findall error');
     }
   }
 
   async findOne(id: number) {
     try {
-      const one = await this.prisma.region.findFirst({where: {id}})
+      const one = await this.prisma.region.findFirst({ where: { id } });
       return one;
     } catch (error) {
-      throw new BadRequestException("Region findone error")
+      throw new BadRequestException('Region findone error');
     }
   }
 
   async update(id: number, data: UpdateRegionDto) {
     try {
-      const one = await this.prisma.region.update({where: {id}, data})
+      const one = await this.prisma.region.update({ where: { id }, data });
       return one;
     } catch (error) {
-      throw new BadRequestException("Region update error")
+      throw new BadRequestException('Region update error');
     }
   }
 
   async remove(id: number) {
     try {
-      const one = await this.prisma.region.delete({where: {id}})
+      const one = await this.prisma.region.delete({ where: { id } });
       return one;
     } catch (error) {
-      throw new BadRequestException("Region delete error")
+      throw new BadRequestException('Region delete error');
     }
   }
 }
