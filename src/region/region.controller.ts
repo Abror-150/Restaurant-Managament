47e56aa64ca09183085349d1b834f791f931e39f
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-05-09 13:42:35 +0500

    changed

--- src/region/region.controller.ts
+++ src/region/region.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
+import { AuthGuard } from 'src/guard/auth.guard';
 
 @Controller('region')
 export class RegionController {
